809165a16ad40e4daa0d01e235aa711294b0f912
Shus13 <sushitkarki234@gmail.com>
2026-06-08 18:15:09 +0545

    basic login done

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,6 @@
 import type { Request, Response } from "express";
 import User from "../database/models/userModel.js";
-
+import bcrypt from 'bcrypt'
 
 
 
@@ -17,12 +17,47 @@ class UserController{
         await User.create({
             username,
             email,
-            password
+            password : bcrypt.hashSync(password,12)
         })
 
         res.status(201).json({
             message : "User registered succesfully"
         })
+    }
+    static async login(req:Request, res:Response){
+        // accept incomming data => email, password
+        const {email,password} = req.body
+
+        if(!email || !password){
+            res.status(400).json({
+                message : "Please provide email,password"
+            })
+            return
+        }
+        // check email existance
+        const [user] = await User.findAll({
+            where : {
+                email : email
+            }
+        })
+        // if yes => check password
+        if(!user){
+            res.status(404).json({
+                message : "No user with that email"
+            })
+        }else{
+            const isEqual = bcrypt.compareSync(password,user.password)
+            if(!isEqual){
+                res.status(400).json({
+                    message : "Invalid password"
+                })
+            }else{
+                res.status(200).json({
+                    message : "Logged in success"
+                })
+            }
+        }
+        // if pass milyo then => token generate garney (jwt)
     }
 }
 
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -7,6 +7,6 @@ const router = express.Router()
 
 
 router.route("/register").post(UserController.register)
-
+router.route("/login").post(UserController.login)
 
 export default router
