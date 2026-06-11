7c06425787ee13a8574acde4c78c449248cdafd6
Shus13 <sushitkarki234@gmail.com>
2026-06-11 09:53:27 +0545

    add category controller

--- server.ts
+++ server.ts
@@ -2,12 +2,12 @@ import adminSeeder from "./adminSeeder.js";
 import app from "./src/app.js";
 
 import { envConfig } from "./src/config/config.js";
-
+import categoryController from "./src/controllers/categoryController.js";
 
 function startServer(){
     const port = envConfig.port || 4000
     app.listen(envConfig.port, ()=>{
-        
+        categoryController.seedCategory()
         console.log(`Server has started at port [${port}]`)
         adminSeeder()
     })
