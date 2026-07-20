c0319685014ef92c81a1be0bcf36405c1dab43eb
Shus13 <sushitkarki234@gmail.com>
2026-07-20 08:50:39 +0545

    verify khalti transaction

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -1,8 +1,8 @@
-import { response, type Request, type Response } from "express";
+import { type Request, type Response } from "express";
 import Order from "../database/models/orderModel.js";
 import OrderDetails from "../database/models/orderDetails.js";
 import Payment from "../database/models/paymentModel.js";
-import { PaymentMethod } from "../globals/types/index.js";
+import { PaymentMethod, PaymentStatus } from "../globals/types/index.js";
 import axios from "axios";
 
 interface IProduct {
@@ -17,7 +17,7 @@ interface OrderRequest extends Request {
 }
 
 class OrderController {
-  async createOrder(req: OrderRequest, res: Response): Promise<void> {
+  static async createOrder(req: OrderRequest, res: Response): Promise<void> {
     const userId = req.user?.id;
     const { phoneNumber, shippingAddress, totalAmount, paymentMethod } =
       req.body;
@@ -75,11 +75,49 @@ class OrderController {
       res.status(200).json({
         message: "Order created successfully",
         url: khaltiResponse.payment_url,
+        pidx : khaltiResponse.pidx
       });
-    } else {
+    } else if(paymentMethod == PaymentMethod.Esewa){
       // esewa logic
+    }else{
+      res.status(200).json({
+        message : "Order created successfully"
+      })
+    }
+  }
+
+  static async verifyTransaction(req:OrderRequest, res:Response):Promise<void>{
+    const {pidx} = req.body
+    if(!pidx){
+        res.status(400).json({
+            message : "Please provide pidx"
+        })
+        return
+    }
+    const response = await axios.post("https://dev.khalti.com/api/v2/epayment/lookup/",{
+        pidx : pidx
+    }, {
+        headers : {
+            "Authorization" : "key f2620d01b1c347e88fc7bf8452e7f4a7",
+        }
+    })
+    console.log(response)
+    const data = response.data
+    if(data.status === "Completed"){
+      await Payment.update({paymentStatus : PaymentStatus.Paid},{
+        where : {
+          pidx : pidx
+        }
+      })
+      res.status(200).json({
+        message : "Payment verified successfuly !!"
+      })
+    }else{
+      res.status(200).json({
+        message : "Payment not verified or cancelled"
+      })
     }
   }
 }
 
-export default new OrderController();
+export default OrderController;
--- src/routes/orderRoute.ts
+++ src/routes/orderRoute.ts
@@ -9,5 +9,6 @@ import errorHandler from '../services/errorHandler.js'
 const router:Router = express.Router()
 
 router.route("/").post(userMiddleware.isUserLoggedIn, errorHandler(orderController.createOrder))
+router.route("/verify-pidx").post(userMiddleware.isUserLoggedIn, errorHandler(orderController.verifyTransaction))
 
 export default router
